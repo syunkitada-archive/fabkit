9e9955e98195c42706d43b805b75d63711a3718f
syunkitada <syun.kitada@gmail.com>
2014-09-13 22:11:16 +0900

    updated

--- setup.sh
+++ setup.sh
@@ -1,14 +1,22 @@
 #!/bin/sh
 
 # Install git
-sudo yum install git
+sudo yum install git -y
 
 
 # Install pip for install fabric
-sudo yum install python-devel libxml2-devel libxslt-devel
+sudo yum install python-devel libxml2-devel libxslt-devel -y
 wget http://python-distribute.org/distribute_setup.py
 sudo python distribute_setup.py
 sudo easy_install pip
 
 # Install fabric
 sudo pip install fabric
+
+
+# init chefric repository and get chefric
+git clone git@github.com:syunkitada/chefric.git
+mkdir nodes
+mkdir fablib
+mkdir fabscript
+cp examples/fabfile.ini ./
