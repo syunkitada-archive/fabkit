ff3e8cafcb43185972aa718fc0735accb6edb102
owner <owner@vagrant-centos65.vagrantup.com>
2014-09-22 21:54:46 +0900

    updated

--- setup.sh
+++ setup.sh
@@ -1,36 +1,45 @@
-#!/bin/sh
+#!/bin/sh -ex
 
 # Install git
 sudo yum install git -y
 
 
 # Install pip for install fabric
-sudo yum install python-devel libxml2-devel libxslt-devel -y
-wget http://python-distribute.org/distribute_setup.py
-sudo python distribute_setup.py
-rm -rf distribute*
-sudo easy_install pip
+which pip
+if [ $? != 0 ]; then
+    sudo yum install python-devel libxml2-devel libxslt-devel -y
+    pushd /tmp
+    wget https://pypi.python.org/packages/source/d/distribute/distribute-0.6.49.tar.gz
+    tar -xvf distribute-0.6.49.tar.gz
+    pushd distribute-0.6.49
+    sudo python setup.py install
+    sudo easy_install pip
+    popd
+    sudo rm -rf distribute*
+    popd
+fi
 
 # Install fabric
 sudo pip install fabric
 
 
+
 # init chefric repository and get chefric
-git clone git@github.com:syunkitada/chefric.git fabfile
-mkdir nodes
-mkdir fablib
-mkdir fabscript
-touch fabscript/__init__.py
-cp fabfile/examples/fabfile.ini ./
+# git clone git@github.com:syunkitada/chefric.git fabfile
+# mkdir nodes
+# mkdir fablib
+# mkdir fabscript
+# touch fabscript/__init__.py
+# cp fabfile/examples/fabfile.ini ./
 
 
-cat << EOF > ./.gitignore
-/fablib/
-/fabfile/
-
-/cookbooks/
-.chef/*.pem
-.chef/data_bag_key
-
-*.pyc
-EOF
+# cat << EOF > ./.gitignore
+# /fablib/
+# /fabfile/
+# 
+# /cookbooks/
+# .chef/*.pem
+# .chef/data_bag_key
+# 
+# *.pyc
+# EOF
