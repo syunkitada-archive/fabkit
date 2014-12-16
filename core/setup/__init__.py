0d62050ee8fc08c25d3cef4f353e1bbda13e45b3
syunkitada <syun.kitada@gmail.com>
2014-12-16 22:40:47 +0900

    add manage task

--- core/setup/__init__.py
+++ core/setup/__init__.py
@@ -59,3 +59,54 @@ def setup(option=None):
             db.setuped(status, msg, script_name=fabscript)
 
     util.dump_node()
+
+
+@task
+@parallel(pool_size=10)
+def manage(*args):
+    if args[0] == 'test':
+        env.is_test = True
+
+    node = env.node_map.get(env.host)
+    node.update({'last_manage': '{0} [start]'.format(util.get_timestamp())})
+    util.dump_node()
+
+    if not check():
+        log.warning('Failed to check(ssh)')
+        return
+
+    filer.mkdir(conf.REMOTE_DIR)
+    filer.mkdir(conf.STORAGE_DIR)
+    filer.mkdir(conf.TMP_DIR, mode='777')
+
+    for fabscript in node.get('fabruns', []):
+        db.setuped(-1, 'start setup', script_name=fabscript)
+        script = '.'.join((conf.FABSCRIPT_MODULE, fabscript))
+        module = __import__(script, {}, {}, 'setup')
+
+        status = 0
+        msgs = []
+        for arg in args:
+            if hasattr(module, arg):
+                func = getattr(module, arg)
+                result = func()
+                if type(result) is IntType:
+                    status = result
+                    msg = arg
+                if type(result) is TupleType:
+                    status, msg = result
+                    msg = '{0}:{1}'.format(arg, msg)
+                if not result:
+                    status = 0
+                    msg = arg
+
+                msgs.append(msg)
+
+                if status != 0:
+                    break
+
+        msg = 'end manage: [{0}]'.format(', '.join(msgs))
+
+        db.setuped(status, msg, script_name=fabscript)
+
+    util.dump_node()
