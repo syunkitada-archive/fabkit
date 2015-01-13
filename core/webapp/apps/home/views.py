e4dc71a0f9ff61414f8626b8cac39689e8b6458e
syunkitada <syun.kitada@gmail.com>
2015-01-13 20:41:11 +0900

    updated

--- core/webapp/apps/home/views.py
+++ core/webapp/apps/home/views.py
@@ -1,19 +1,10 @@
 # coding: utf-8
 
-from django.shortcuts import render, redirect
-from django.contrib.auth import authenticate, login, logout
-from django.contrib.auth.models import User
-from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm, UserCreationForm
+from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 
 
 @login_required
 def index(request):
-    context = {
-        'title': 'Home',
-    }
-
-    if request.META.get('HTTP_X_PJAX'):
-        return render(request, 'home/content.html', context)
-
-    return render(request, 'home/index.html', context)
+    return redirect(reverse('node:index'))
