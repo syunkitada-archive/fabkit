c1f7535dd5b7728680d9c6d908ef4f99486d6356
owner <owner@dev01.vagrant.mydns.jp>
2015-01-18 19:28:39 +0900

    update examples

--- examples/setup_httpd_for_centos.sh
+++ examples/setup_httpd_for_centos.sh
@@ -4,12 +4,18 @@ sudo yum install httpd mod_wsgi -y
 sudo cp fabfile/examples/httpd_vhost.conf /etc/httpd/conf.d/
 sudo mkdir -p /opt/fabkit/conf
 sudo cp fabfile/examples/fabkit.wsgi /opt/fabkit/conf/
-sudo mkdir -p /opt/fabkit/log
+sudo mkdir -p /opt/fabkit/storage/log
+sudo mkdir -p /opt/fabkit/storage/dump
 
-if [ ! -e /opt/fabkit/webapp ]; then
-    sudo ln -s `pwd`/fabfile/core/webapp /opt/fabkit/webapp
+if [ ! -e /opt/fabkit/fabfile ]; then
+    if [ $# -e 1 -a $1 -e link ]; then
+        sudo ln -s `pwd`/fabfile/ /opt/fabkit/fabfile
+        sudo chmod 701 /home/`whoami`
+    else
+        sudo cp -r fabfile/ /opt/fabkit/fabfile
+        sudo chown -R apache:apache /opt/fabkit
+    fi
 fi
 
-sudo chmod 701 /home/`whoami`
 sudo service httpd restart
 sudo chkconfig httpd on
